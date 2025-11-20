95937a6975d859f852e864ba26e0fb68450c94e3
zhouruijin <zhruji@gmail.com>
2025-11-20 11:10:05 +0800

    ss

--- src/main/java/com/alibaba/cloud/ai/manus/tool/jira/client/JiraClientV2.java
+++
@@ -1,7 +0,0 @@
-package com.alibaba.cloud.ai.manus.tool.jira.client;
-
-public class JiraClientV2 extends JiraAbstractClient {
-    {
-        PREFIX = "/rest/api/2";
-    }
-}
--- src/main/java/com/alibaba/cloud/ai/manus/tool/jira/client/JiraClientV3.java
+++
@@ -1,7 +0,0 @@
-package com.alibaba.cloud.ai.manus.tool.jira.client;
-
-public class JiraClientV3 extends JiraAbstractClient {
-    {
-        PREFIX = "/rest/api/3";
-    }
-}
